f80a975fe12e97ac3264428386791dcea7fe32b8
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-04 02:33:55 +0530

    Inserting & querying data

--- 03-persistance/main.go
+++ 03-persistance/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -21,18 +22,49 @@ func main() {
 	}
 
 	//Creating a new table
-	query := `
-		CREATE TABLE users (
-			id INT NOT NULL AUTO_INCREMENT,
-			username TEXT NOT NULL,
-			password TEXT NOT NULL,
-			created_at DATETIME,
-			PRIMARY KEY (id)
-		);
-	`
-	_, err = db.Exec(query)
+	/*
+		query := `
+			CREATE TABLE users (
+				id INT NOT NULL AUTO_INCREMENT,
+				username TEXT NOT NULL,
+				password TEXT NOT NULL,
+				created_at DATETIME,
+				PRIMARY KEY (id)
+			);
+		`
+		_, err = db.Exec(query)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println("table created")
+	*/
+
+	//Inserting a new user
+	/*
+		username := "testuser"
+		password := "testpassword"
+		created_at := time.Now()
+
+		result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, created_at)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		id, err := result.LastInsertId()
+		fmt.Printf("User with id %d is created\n", id)
+	*/
+
+	//Selecting a user
+	var (
+		id        int
+		username  string
+		password  string
+		createdAt time.Time
+	)
+	query := `SELECT id, username, password, created_at FROM users WHERE id = ?`
+	err = db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println("table created")
+	fmt.Println(id, username, password, createdAt)
+
 }
