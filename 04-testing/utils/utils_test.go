d2742be47d1128fd9a6210e7ba0f32a5f44f47e7
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-04 19:22:12 +0530

    table driven tests

--- 04-testing/utils/utils_test.go
+++ 04-testing/utils/utils_test.go
@@ -30,10 +30,11 @@ func TestIsNotPrime(t *testing.T) {
 	}
 } */
 
-func TestUtils(t *testing.T) {
+//sub tests
+/* func TestUtils(t *testing.T) {
 	t.Run("Number Tests", func(t *testing.T) {
 		t.Run("Prime Test", func(t *testing.T) {
-			t.Run("Testing Prime", func(t *testing.T) {
+			t.Run("Testing Prime for 11", func(t *testing.T) {
 				//Arrage
 				no := 11
 				expectedResult := true
@@ -47,9 +48,10 @@ func TestUtils(t *testing.T) {
 				}
 			})
 
-			t.Run("Testing Not Prime", func(t *testing.T) {
-				no := 100
-				expectedResult := false
+			t.Run("Testing Prime for 13", func(t *testing.T) {
+				//Arrage
+				no := 11
+				expectedResult := true
 
 				//Act
 				result := IsPrime(no)
@@ -58,12 +60,61 @@ func TestUtils(t *testing.T) {
 				if result != expectedResult {
 					t.Fail()
 				}
+
+				t.Run("Testing Not Prime", func(t *testing.T) {
+					no := 100
+					expectedResult := false
+
+					//Act
+					result := IsPrime(no)
+
+					//Assert
+					if result != expectedResult {
+						t.Fail()
+					}
+				})
+			})
+
+			t.Run("Even Tests", func(t *testing.T) {
+				if IsEven(10) != true {
+					t.Fail()
+				}
 			})
 		})
+	})
+} */
 
-		t.Run("Even Tests", func(t *testing.T) {
-			if IsEven(10) != true {
-				t.Fail()
+//table drive tests
+
+type PrimeTestCase struct {
+	name           string
+	no             int
+	expectedResult bool
+}
+
+func TestUtils(t *testing.T) {
+	t.Run("Number Tests", func(t *testing.T) {
+		t.Run("Prime Test", func(t *testing.T) {
+			testCases := []PrimeTestCase{
+				{"Testing Prime for 11", 11, true},
+				{"Testing Prime for 13", 13, true},
+				{"Testing Prime for 17", 17, true},
+				{"Testing Prime for 19", 19, true},
+			}
+
+			for _, testCase := range testCases {
+				t.Run(testCase.name, func(t *testing.T) {
+					//Arrage
+					no := testCase.no
+
+					//Act
+					result := IsPrime(no)
+
+					//Assert
+					if result != testCase.expectedResult {
+						t.Fail()
+					}
+				})
 			}
 		})
 	})
